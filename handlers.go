eaa17a6a15acce3db2bb9dfb69ae0c19968bf9c7
Thomas McDonald <thomasmcdonald@Thomass-MacBook-Pro.local>
2018-09-15 12:44:54 -0500

    Added YAML support

--- handlers.go
+++ handlers.go
@@ -2,21 +2,55 @@ package urlshortener
 
 import (
 	"net/http"
-	// "html/template"
-	//"gopkg.in/yaml.v2"
+	"fmt"
+	yaml "gopkg.in/yaml.v2"
 )
 
-func MapHandler(paths map[string]string) http.HandlerFunc {
+func MapHandler(paths map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
-		// get the extension from req
 		reqPath := req.URL.Path[1:]
 
 		fullPath, ok := paths[reqPath]
 		if !ok {
-			res.WriteHeader(http.StatusNotFound)
-			res.Write([]byte("Page Not Found"))
+			if fallback == nil {
+				http.NotFound(res, req)
+			} else {
+				fallback.ServeHTTP(res, req)
+			}
 			return
 		}
 		http.Redirect(res, req, fullPath, http.StatusFound)
 	}
+}
+
+// Yaml format is - path: test
+//					url: test.com
+func YamlHandler(yamlIn []byte, fallback http.Handler) http.HandlerFunc {
+	m, err := parseYaml(yamlIn)
+	if err != nil {
+		fmt.Println(err)
+		return fallback.ServeHTTP
+	}
+	return MapHandler(m, fallback)
+}
+
+func parseYaml(yamlIn []byte) (map[string]string, error) {
+	var pathList []path
+	m := make(map[string]string)
+	err := yaml.Unmarshal(yamlIn, &pathList)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create map from list
+	for _, p := range(pathList) {
+		m[p.Path] = p.URL
+	}
+	fmt.Println(m)
+	return m, nil
+}
+
+type path struct {
+	Path string `yaml:"path"`
+	URL string `yaml:"url"`
 }
--- main/main.go
+++ main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"fmt"
+	// "html/template"
 	urlshort "github.com/tmcdo1/url-shortener"
 )
 
@@ -11,16 +12,35 @@ var (
 )
 
 func main() {
-	// Get a mux
-	// mux := http.NewServeMux()
+	// Default mux used as a fallback
+	mux := baseMux()
 
 	// Get routes and redirect route
 	paths := map[string]string {
 		"personal": "https://tmcdo1.github.io",
 	}
+	y := `
+- path: test
+  url: https://google.com
+`
 	// Get
-	handler := urlshort.MapHandler(paths)
+	mapHandler := urlshort.MapHandler(paths, mux)
+	handler := urlshort.YamlHandler([]byte(y), mapHandler)
 	// Start up HTTP server
 	fmt.Printf("Your url-shortener server is running on port %d\n", PORT)
 	http.ListenAndServe(fmt.Sprintf(":%d", PORT), handler)
+}
+
+func baseMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", baseHandle)
+	return mux
+}
+
+func baseHandle(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		w.Write([]byte("Welcome to the URL-Shortener"))
+	} else {
+		http.NotFound(w, r)
+	}
 }
